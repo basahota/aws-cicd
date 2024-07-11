e1d876279c82567e2a3640e4cc5071032fd3dcaf
javatechie <basanta.kumar.hota.4@gmail.com>
2024-07-11 16:15:19 +0530

    disable health check

--- src/main/java/com/javatechie/controller/CourseController.java
+++ src/main/java/com/javatechie/controller/CourseController.java
@@ -57,10 +57,10 @@ public class CourseController {
         }
     }
 
-    @GetMapping("/health")
-    public String healthCheck() {
-        return "UP";
-    }
+//    @GetMapping("/health")
+//    public String healthCheck() {
+//        return "UP";
+//    }
 
 
 }
